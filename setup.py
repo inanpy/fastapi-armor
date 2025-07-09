8adeb1ce39003e840108b95d4ee3a726b1235765
inanpy <inanndelibas@gmail.com>
2025-07-09 21:51:03 +0200

    chore: bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastapi-armor",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         "fastapi>=0.115.13",
